15835be370eccb3a488b0da5e028c10c847ea663
RamPrakash888 <86499358+RamPrakash888@users.noreply.github.com>
2021-06-28 18:01:57 +0530

    Add files via upload

---
+++ Assets/inti.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using GoogleMobileAds.Api;
+
+public class inti : MonoBehaviour
+{
+    string appid = "ca-app-pub-2001875684419121~2888396338";
+    private InterstitialAd interstitial;
+    string adu= "ca-app-pub-2001875684419121/4748272917";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MobileAds.Initialize(appid);
+        
+        RequestInterstitial();
+        showinter();
+
+    }
+    public void RequestInterstitial()
+    {
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adu);
+        // Called when an ad request has successfully loaded.
+        this.interstitial.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is shown.
+        this.interstitial.OnAdOpening += HandleOnAdOpened;
+        // Called when the ad is closed.
+        this.interstitial.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.interstitial.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+
+        AdRequest request = new AdRequest.Builder().Build();
+        this.interstitial.LoadAd(request);
+        
+
+    }
+    public void showinter()
+    {
+        Debug.Log("shown");
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+
+        }
+    }
+    
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+        this.interstitial.Show();
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
+    }
+}
---
+++ Assets/music.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Video;
+
+public class music : MonoBehaviour
+{
+    public AudioSource c1;
+    public AudioSource d1;
+    public AudioSource e1;
+    public AudioSource f1;
+    public AudioSource g1;
+    public AudioSource a1;
+    public AudioSource b1;
+    public AudioSource c2;
+    public AudioSource d2;
+    public AudioSource e2;
+    public AudioSource f2;
+    public AudioSource g2;
+    public AudioSource a2;
+    public AudioSource b2;
+    public AudioSource c1a;
+    public AudioSource d1a;
+    public AudioSource f1a;
+    public AudioSource g1a;
+    public AudioSource a1a;
+    public AudioSource c2a;
+    public AudioSource d2a;
+    public AudioSource f2a;
+    public AudioSource g2a;
+    public AudioSource a2a;
+    
+    
+
+    public void plc1()
+    {
+        c1.Play();
+
+    }
+    
+
+    public void pld1()
+    {
+        d1.Play();
+
+    }
+    public void ple1()
+    {
+        e1.Play();
+
+    }
+    public void plf1()
+    {
+        f1.Play();
+
+    }
+    public void plg1()
+    {
+        g1.Play();
+
+    }
+    public void pla1()
+    {
+        a1.Play();
+
+    }
+    public void plb1()
+    {
+        b1.Play();
+
+    }
+    public void plc2()
+    {
+        c2.Play();
+
+    }
+    public void pld2()
+    {
+        d2.Play();
+
+    }
+    public void ple2()
+    {
+        e2.Play();
+
+    }
+    public void plf2()
+    {
+        f2.Play();
+
+    }
+    public void plg2()
+    {
+        g2.Play();
+
+    }
+    public void pla2()
+    {
+        a2.Play();
+
+    }
+    public void plb2()
+    {
+        b2.Play();
+
+    }
+    public void plc1a()
+    {
+        c1a.Play();
+
+    }
+    public void pld1a()
+    {
+        d1a.Play();
+
+    }
+    public void plf1a()
+    {
+        f1a.Play();
+
+    }
+    public void plg1a()
+    {
+        g1a.Play();
+
+    }
+    public void pla1a()
+    {
+        a1a.Play();
+
+    }
+    public void plc2a()
+    {
+        c2a.Play();
+
+    }
+    public void pld2a()
+    {
+        d2a.Play();
+
+    }
+    public void plf2a()
+    {
+        f2a.Play();
+
+    }
+    public void plg2a()
+    {
+        g2a.Play();
+
+    }
+    public void pla2a()
+    {
+        a2a.Play();
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
